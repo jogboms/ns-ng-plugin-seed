a1f5a02edadcb8260ddfad12c908fd297dab6579
Jogboms <jeremiahogbomo@gmail.com>
2017-06-22 21:04:07 +0100

    Fix error with templateUrl value

--- src/test.component.ts
+++ src/test.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, OnDestroy } from "@angular/core";
 @Component({
   selector: "test",
   styleUrls: ['./test.css'],
-  templateUrl: './test.component',
+  templateUrl: './test.component.html',
 })
 export class TestComponent implements OnInit, OnDestroy {
 
