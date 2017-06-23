4a5b363631aab5afe8d58364976b2b0fc33d85df
Jogboms <jeremiahogbomo@gmail.com>
2017-06-23 05:40:58 +0100

    Wheeew... moduleId

--- src/test.component.ts
+++ src/test.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 
 @Component({
+  moduleId: module.id,
   selector: "test",
   styleUrls: ['./test.css'],
   templateUrl: './test.component.html',
